8d08ca00355b5a5f864f49fc8f6fe673c33f5368
Moe Yan Htun <moeyan227970@gmail.com>
2024-03-12 19:35:21 +0630

    post delet and conditional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import { useState } from 'react'
 function App() {
   let [name, setName] = useState('Moe Yan Htun');
+  let [posts, setPost] = useState([
+    {
+      id: 1,
+      title: 'post 1'
+    },
+    {
+      id: 2,
+      title: 'post 2'
+    },
+    {
+      id: 3,
+      title: 'post 3'
+    }
+  ])
   let changeName = () => {
     setName('Yan Yan');
+  }
+  let deletePost = (id) => {
+    setPost((prevState) => prevState.filter(post => post.id !== id));
   }
   return (
     <div className="App">
       <button onClick={changeName}>ChangeName</button>
       <h1>{name}</h1>
+      <ul>
+        {!!posts.length && posts.map((post) => (
+          <li key={post.id}>{post.title}
+            <button onClick={() => deletePost(post.id)}>Delete</button>
+          </li>
+        ))}{
+          !posts.length && <p>There are no post</p>
+        }
+      </ul>
     </div>
   );
 }
