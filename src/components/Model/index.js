47ab22354c73cf97b1591d30c8a7bbd166d335ca
Moe Yan Htun <moeyan227970@gmail.com>
2024-03-13 20:58:02 +0630

    react scope css

--- src/components/Model/index.js
+++ src/components/Model/index.js
@@ -5,9 +5,11 @@ import ReactDOM from 'react-dom'
 export default function model({ children }) {
     return (
         ReactDOM.createPortal(
-            <div className='model-backdrop'>
-                <div className='model'>
-                    {children}
+            <div className='model-component'>
+                <div className='model-backdrop'>
+                    <div className='model'>
+                        {children}
+                    </div>
                 </div>
             </div>
             , document.getElementById('model')
